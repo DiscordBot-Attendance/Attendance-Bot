7c40b89430b046efce996502a6e6244a7e093a47
webstriix <webstriix@gmail.com>
2025-02-13 20:25:31 +0700

    add register and login command help

--- src/config/constant.rs
+++ src/config/constant.rs
@@ -1,8 +1,9 @@
-pub const HELP_MESSAGES:&str = "
+pub const HELP_MESSAGES: &str = "
 Hi! I'm the Attendance Bot.
 
 Commands:
-!AB register                                -   Login as Admin team
+!AB register {password}                     -   Register as Admin team
+!AB login {password}                        -   Login as Admin team
 !AB create_team {team name}                 -   Create new team
 !AB add_member {team_name} {member_name}    -   Add member to team
 !AB check_in {team_name}                    -   Start session
