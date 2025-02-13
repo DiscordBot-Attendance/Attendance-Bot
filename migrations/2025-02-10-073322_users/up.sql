a437f174296054b1b2afbff462a2c0aaf3994bb5
webstriix <webstriix@gmail.com>
2025-02-13 12:42:59 +0700

    add discord_id

--- migrations/2025-02-10-073322_users/up.sql
+++ migrations/2025-02-10-073322_users/up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
+    discord_id VARCHAR(50) NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     is_admin BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
