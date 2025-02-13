4d0aa09aa79929f0ebbcbff9921b381095556c5b
webstriix <webstriix@gmail.com>
2025-02-13 20:34:11 +0700

    set the bot to main function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,36 @@
-use std::env;
-
-use dotenvy::dotenv;
-
+mod bot;
 mod config;
+mod schema;
+
+use bot::adapters::discord_bot::Handler;
+use config::{database, logger, settings};
+use serenity::prelude::*;
 
 #[tokio::main]
 async fn main() {
-    print!("Hello world!");
+    // Initialize Logger
+    logger::init_logger();
+    log::info!("Starting Attendance Bot...");
+
+    // Load App Configuration
+    let config = settings::Settings::new();
+    log::info!("Configuration loaded!");
+
+    // Connect to the Database
+    let db_pool = database::establish_connection();
+    log::info!("Database connection established!");
+
+    // Initialize Discord Client
+    let token = config.discord_token;
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler::new(db_pool.clone()))
+        .await
+        .expect("Error creating Discord client");
+
+    // Start the Bot
+    if let Err(e) = client.start().await {
+        log::error!("Client error: {:?}", e);
+    }
 }
