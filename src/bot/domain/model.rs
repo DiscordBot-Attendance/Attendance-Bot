0f8dddce6fb07bfb40e768547ba4aeebcf157eea
webstriix <webstriix@gmail.com>
2025-02-13 20:27:40 +0700

    create model for users, teams, members

---
+++ src/bot/domain/model.rs
@@ -0,0 +1,48 @@
+use crate::schema::member_attendance;
+use crate::schema::members;
+use crate::schema::teams;
+use crate::schema::users;
+
+use chrono::NaiveDateTime;
+use diesel::prelude::*;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Queryable)]
+pub struct User {
+    pub id: i32,
+    pub discord_id: String,
+    pub username: String,
+    pub password_hash: String,
+    pub is_admin: Option<bool>,
+    pub created_at: Option<NaiveDateTime>,
+    pub updated_at: Option<NaiveDateTime>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Insertable)]
+#[table_name = "users"]
+pub struct NewUser {
+    pub discord_id: String,
+    pub username: String,
+    pub password_hash: String,
+    pub is_admin: bool,
+    pub created_at: chrono::NaiveDateTime,
+}
+
+#[derive(Debug, Serialize, Deserialize, Queryable, Insertable)]
+#[table_name = "teams"]
+pub struct Team {
+    pub id: i32,
+    pub name: String,
+    pub admin_id: i32,
+    pub created_at: chrono::NaiveDateTime,
+}
+
+// #[derive(Debug, Serialize, Deserialize, Queryable)]
+// #[table_name = "members"]
+// pub struct Member {
+//     pub id: i32,
+//     pub team_id: i32,
+//     pub name: String,
+//     pub position: String,
+//     pub join_date: chrono::NaiveDateTime,
+// }
