108b4f3f46ec889966ab3e373439d8a90757dea6
webstriix <webstriix@gmail.com>
2025-02-15 17:59:02 +0700

    change the name to discord_id at members table

--- migrations/2025-02-10-073942_members/up.sql
+++ migrations/2025-02-10-073942_members/up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE members (
     id SERIAL PRIMARY KEY,
     team_id INTEGER REFERENCES teams(id) ON DELETE CASCADE,
-    name VARCHAR(50) NOT NULL,
+    discord_id VARCHAR(50) NOT NULL,
     position VARCHAR(50),
     join_date DATE DEFAULT CURRENT_DATE
 );
