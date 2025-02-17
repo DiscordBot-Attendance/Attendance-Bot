c7b49adcf25ba406ad01482c6dbbdf8b87ff0108
webstriix <webstriix@gmail.com>
2025-02-17 16:38:57 +0700

    call repository to show_team service

--- src/bot/application/services/team_service.rs
+++ src/bot/application/services/team_service.rs
@@ -1,4 +1,4 @@
-use crate::bot::infrastructure::persistence::team_repository;
+use crate::bot::{domain::table::TeamTable, infrastructure::persistence::team_repository};
 use diesel::PgConnection;
 
 pub fn register_team(conn: &mut PgConnection, name: &str, admin_id: i32) -> Result<(), String> {
@@ -7,4 +7,11 @@ pub fn register_team(conn: &mut PgConnection, name: &str, admin_id: i32) -> Resu
 
 pub fn add_member(conn: &mut PgConnection, discord_id: &str, team_id: i32) -> Result<(), String> {
     team_repository::assign_member(conn, discord_id, team_id)
+}
+
+pub fn show_team(
+    conn: &mut PgConnection,
+    admin_discord_id: &str,
+) -> Result<Vec<TeamTable>, String> {
+    team_repository::get_admin_teams(conn, admin_discord_id)
 }
