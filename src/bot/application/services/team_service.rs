02cd6a148e73127cd866863a8d9f7d5d8ed3ade6
webstriix <webstriix@gmail.com>
2025-02-19 11:12:08 +0700

    create get_members_by_team and edit add_member
    - add username parameter to the add_member
    - add implement the get_members to the get_members_by_team function

--- src/bot/application/services/team_service.rs
+++ src/bot/application/services/team_service.rs
@@ -1,12 +1,20 @@
-use crate::bot::{domain::table::TeamTable, infrastructure::persistence::team_repository};
+use crate::bot::{
+    domain::table::{MemberTable, TeamTable},
+    infrastructure::persistence::team_repository::{self},
+};
 use diesel::PgConnection;
 
 pub fn register_team(conn: &mut PgConnection, name: &str, admin_id: i32) -> Result<(), String> {
     team_repository::create_team(conn, name, admin_id)
 }
 
-pub fn add_member(conn: &mut PgConnection, discord_id: &str, team_id: i32) -> Result<(), String> {
-    team_repository::assign_member(conn, discord_id, team_id)
+pub fn add_member(
+    conn: &mut PgConnection,
+    discord_id: &str,
+    username: String,
+    team_id: i32,
+) -> Result<(), String> {
+    team_repository::assign_member(conn, discord_id, username, team_id)
 }
 
 pub fn show_team(
@@ -14,4 +22,11 @@ pub fn show_team(
     admin_discord_id: &str,
 ) -> Result<Vec<TeamTable>, String> {
     team_repository::get_admin_teams(conn, admin_discord_id)
+}
+
+pub fn get_members_by_team(
+    conn: &mut PgConnection,
+    team_name: &str,
+) -> Result<Vec<MemberTable>, String> {
+    team_repository::get_members(conn, team_name)
 }
