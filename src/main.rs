6a682d8d0f0e8ea1e268c6acd00307790c65e6e6
webstriix <webstriix@gmail.com>
2025-02-25 17:58:46 +0700

    run the api and bot simultaneously

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 mod bot;
 mod config;
 mod schema;
+mod api;
 
+use actix_web::rt::System;
 use bot::adapters::discord_bot::Handler;
+use api::adapters::http_server::{self, *};
 use config::{database, logger, settings};
 use serenity::prelude::*;
+use tokio::task;
 
 #[tokio::main]
 async fn main() {
@@ -20,8 +24,20 @@ async fn main() {
     let db_pool = database::establish_connection();
     log::info!("Database connection established!");
 
+    // run discord bot and actix api in parallel
+    let bot_discord  = task::spawn(run_discord_bot(db_pool.clone(), config.discord_token));
+    let api_discord = task::spawn(run_api_server());
+
+    // wait for both task complete
+    let _ = tokio::join!(bot_discord, api_discord);
+}
+
+// function to start the discord bot
+async fn run_discord_bot(
+    db_pool: diesel::r2d2::Pool<diesel::r2d2::ConnectionManager<diesel::PgConnection>>, 
+    token: String) 
+{
     // Initialize Discord Client
-    let token = config.discord_token;
     let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
 
     let mut client = Client::builder(&token, intents)
@@ -33,4 +49,12 @@ async fn main() {
     if let Err(e) = client.start().await {
         log::error!("Client error: {:?}", e);
     }
+
+}
+
+// function to start the API server
+async fn run_api_server() {
+    System::new().block_on(async {
+        http_server::start_api().await;
+    });
 }
